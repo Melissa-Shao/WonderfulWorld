047edbd4e52e3397e9a8b7509d618b1db0f8bd20
Melissa Shao <mshao8@my.bcit.ca>
2024-09-19 14:30:41 -0700

    Initialize the repo

---
+++ README.md
@@ -0,0 +1,25 @@
+# COMP 2522 Term Project
+
+## Project member
+---
+- [Melissa-Shao](https://github.com/Melissa-Shao)</br>
+- [candiceweily](https://github.com/candiceweily)
+
+## Student number
+---
+- A01386061</br>
+- A01386861
+
+
+## Project Description
+---
+
+## Technical Requirements
+---
+- **IDE**: IntelliJ IDEA
+- **Java SDK**: 22
+- **Build Tool**: Maven
+- **Test Framework**: JUnit 5
+
+
+
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module io.github.wonderfulworld.comp2522202430termprojectwonderfulworld {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens io.github.wonderfulworld.comp2522202430termprojectwonderfulworld to javafx.fxml;
+    exports io.github.wonderfulworld.comp2522202430termprojectwonderfulworld;
+}

a79c26e4f3b87b05c440640a5cbf176473b1d30a
Candice Wei <weily00@bu.edu>
2024-11-24 06:48:24 -0800

    Add json dependency in Maven

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module io.github.wonderfulworld.comp2522202430termprojectwonderfulworld {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires static lombok;
+    requires org.json;
 
     opens io.github.wonderfulworld.comp2522202430termprojectwonderfulworld to javafx.fxml;
     exports io.github.wonderfulworld.comp2522202430termprojectwonderfulworld;
